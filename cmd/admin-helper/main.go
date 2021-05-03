5c88e3ea2c2e192992095b3582595e6f4fb0ade7
Guillaume Rose <gurose@redhat.com>
2021-05-03 11:28:26 +0200

    Move main.go and code related to the binary in the same directory
    Preparatory work to have one more binary in the repo.

--- cmd/admin-helper/main.go
+++ cmd/admin-helper/main.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/code-ready/admin-helper/cmd"
 	"github.com/spf13/cobra"
 )
 
 var Version = "dev"
 
 func main() {
+	commands := []*cobra.Command{
+		Add,
+		Remove,
+		Clean,
+		Contains,
+	}
+
 	rootCmd := &cobra.Command{
 		Use:          "admin-helper",
 		Version:      Version,
 		SilenceUsage: true,
 	}
 
-	rootCmd.AddCommand(cmd.Commands()...)
+	rootCmd.AddCommand(commands...)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
